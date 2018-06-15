19935aed9985640b841efa8a4dc72a6750628963
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-15 15:13:23 +0200

    Ajout de l'exo du nombre aléatoire

--- jquery-exercices-base3-master/app.js
+++ jquery-exercices-base3-master/app.js
@@ -12,3 +12,25 @@ $('#moins').click( function (){
 		count1--;
 	$('#compteur1').html(count1);
 });
+var min = 0;
+var max=100;
+var cpt=0;
+var nb =Math.floor( min + (max-min+1)*Math.random());
+	console.log(nb);
+
+$('#validate').click(function() {
+
+	var playerNb= $("#playerNb").val();
+		console.log(playerNb);
+		cpt++
+	if (playerNb == nb) {
+		$("#result").html("Bravo, tu as trouvé au bout de "+cpt+" fois");
+		$("#playerNb").val('');
+	}else if (playerNb < nb) {
+		$("#result").html("C'est plus grand");
+		$("#playerNb").val('');
+	}else if (playerNb > nb) {
+		$("#result").html("C'est plus petit");
+		$("#playerNb").val('');
+	}
+});
