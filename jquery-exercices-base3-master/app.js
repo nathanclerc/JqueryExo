61be74d66e2b3adab5692f2ab7332bf4e4fbb7d7
SIMOCCAuch19 <clerc6nathan@hotmail.fr>
2018-06-15 14:54:12 +0200

    ajout des bouttons et fonctionnalité plus et moins avec le compteur qui leurs est associé

--- jquery-exercices-base3-master/app.js
+++ jquery-exercices-base3-master/app.js
@@ -3,3 +3,12 @@ $('#boutton').click( function (){
 		count++;
 	$('#compteur').html(count);
 });
+	var count1 =0;
+$('#plus').click( function (){
+		count1++;
+	$('#compteur1').html(count1);
+});
+$('#moins').click( function (){
+		count1--;
+	$('#compteur1').html(count1);
+});
